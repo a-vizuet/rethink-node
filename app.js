f2c110e4e65042d1a8b6071fd4f8492d40397544
megustalafantabienfria <megustalafantabienfria@hotmail.com>
2019-04-23 13:59:47 -0500

    Now we can handle our db connection!

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const app = require('express')();
+const rethink = require('rethinkdb');
+
+app.all('/', async (req, res) => {
+  try {
+    const allData = await rethink.table('test').getAll().run(require('./index')());
+    
+    res.send({allData});
+  } catch (error) {
+    res.send({error: error.msg});
+  }
+});
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const app = require('./app');
 const rethink = require('rethinkdb');
+let connection = null;
 
 (async () => {
-
   try {
-    await rethink.connect({ host: '127.0.0.1', port: 28015 });
+    connection = await rethink.connect({ host: '127.0.0.1', port: 28015 });
     await app.listen(process.env.PORT || 3030);
 
     console.log('Servidor escuchando!');
   } catch (error) {
     console.log(error);
   }
+
 })();
+
+function getConnection() {
+  return connection;
+}
+
+module.exports = getConnection;
